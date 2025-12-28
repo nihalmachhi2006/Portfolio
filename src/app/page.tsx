30c2c4b94bc15b870d698dfbca5aa6011056b3c9
Nihal Machhi <nihalmachhi11@gmail.com>
2025-12-28 09:18:37 +0530

    Start Portfolio Buiding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div>
-        <h1>Deployed Successfully : 200</h1>
+        <h1>Push To Production successfully : 200</h1>
       </div>
     </div>
   );
